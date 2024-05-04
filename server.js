54f049b08cc33c3760372507cdbc33efd1c218fc
mohitx1 <mohitrockx3@gmail.com>
2024-05-04 17:34:33 +0530

    database connected

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+
+//database connection
+const db_connection = require('./db')
+db_connection()
+
+// get config vars
+dotenv.config();
+const port=process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send(`<h1> Server started </h1>`)
+    res.send(`Welcome to my hotel... How can ihelp you?`)
+});
+
+app.get('/chicken',(req,res)=>{
+    res.send(`Love to serve you chicken`)
+});
+
+app.get('/idli',(req,res)=>{
+    let idli ={
+        name: 'idli',
+        size: '10cm diameter',
+        id_chutney: true
+    }
+    res.send(idli)
+});
+
+app.post('/items',(req,res)=>{
+    console.log('posted')
+    res.send('posted data')
 })
 
 
-app.listen(PORT,()=>{
-    console.log(`Server running on Port ${PORT}`)
-})
+app.listen(port,()=>{
+    console.log(`Server running on Port ${port}`)
+});
