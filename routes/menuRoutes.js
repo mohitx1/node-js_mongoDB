dc573a42fd124f31951365997cee581cbe7009a0
mohitx1 <mohitrockx3@gmail.com>
2024-05-04 20:01:04 +0530

    menu route also created

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const person = require('../models/person');
+const menuItems = require('../models/menu');
 
 
 router.post('/', async (req, res) => {
